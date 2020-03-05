45fd77ea98932cc181474ae39e71b3656157fc65
chenxeed <chenxeed@gmail.com>
2020-03-06 00:31:24 +0800

    fix: error on reducer while build

--- client/src/app/store/bill/reducer.ts
+++ client/src/app/store/bill/reducer.ts
@@ -1,13 +1,13 @@
 import * as actions from './actions';
 import { Bill } from 'src/app/bill';
-import { InitialState } from '@ngrx/store/src/models';
+import { InitialState, Action } from '@ngrx/store/src/models';
 import { createReducer, on } from '@ngrx/store';
 
 type State = Bill[];
 
 export const initialState: InitialState<State> = [];
 
-export const billReducer = createReducer(
+const reducer = createReducer(
   initialState,
   on(actions.InitBills, (_state, { payload }) => {
     return payload;
@@ -33,5 +33,9 @@ export const billReducer = createReducer(
     return state.filter(bill => bill.id !== payload);
   })
 );
+
+export function billReducer (state: State | undefined, action: Action) {
+  return reducer(state, action)
+}
 
 export const selectBills = (state: State) => state;
