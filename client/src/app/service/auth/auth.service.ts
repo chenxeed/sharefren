6f779d346180efb5efe60b0cbbd4b8fbcda5e857
chenxeed <chenxeed@gmail.com>
2020-03-23 01:13:38 +0800

    feat: link anonymous user on login to google

--- client/src/app/service/auth/auth.service.ts
+++ client/src/app/service/auth/auth.service.ts
@@ -3,6 +3,19 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
 import { Observable } from 'rxjs/internal/Observable';
 
+
+interface GoogleUserInfo {
+  name: string,
+  granted_scopes: string,
+  id: string,
+  verified_email: boolean,
+  given_name: string,
+  locale: string,
+  family_name: string,
+  email: string,
+  picture: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +25,7 @@ export class AuthService {
   userDetails: firebase.User = null;
 
   constructor(private _firebaseAuth: AngularFireAuth) {
-    this.user$ = _firebaseAuth.authState;
+    this.user$ = _firebaseAuth.user;
     this.user$.subscribe(async user => {
       if (user) {
         this.userDetails = user;
@@ -27,9 +40,17 @@ export class AuthService {
 
   async loginByGoogle () {
     await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-    return this._firebaseAuth.auth.signInWithPopup(
-      new firebase.auth.GoogleAuthProvider()
-    )
+    const provider = new firebase.auth.GoogleAuthProvider()
+    const link = await this._firebaseAuth.auth.currentUser.linkWithPopup(provider);
+    // link the user profile with the google data
+    const additionalUserInfo = link.additionalUserInfo.profile as GoogleUserInfo
+    await firebase.auth().currentUser.updateProfile({
+      photoURL: additionalUserInfo.picture,
+      displayName: additionalUserInfo.given_name
+    })
+    // run this to trigger the user$ with the latest user data
+    this._firebaseAuth.auth.updateCurrentUser(this._firebaseAuth.auth.currentUser)
+    return link
   }
 
   logout () {
